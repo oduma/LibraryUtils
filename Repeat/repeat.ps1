822fba4372ac10061c0ed17b0585720fd5a5b744
oduma <tavi_duma2002@yahoo.co.uk>
2017-10-11 22:44:39 +0100

    added the filter for folders not finished yet

--- Repeat/repeat.ps1
+++ Repeat/repeat.ps1
@@ -11,7 +11,71 @@ if([string]::IsNullOrEmpty($archiveExtension))
 {
     $archiveExtension=".zip"
 }
-$folders = Get-ChildItem -Path $startFolder -Exclude $targetFolder | ?{ $_.PSIsContainer }
+
+function GetFileName([string] $startFolder, [string] $torrentFolder, [string] $torrentContentLine)
+{
+    $torrentLineParts = $torrentContentLine.Split('(')
+    $torrentFileName=$torrentLineParts[0].Trim()
+    $torrentFileName = $torrentFileName -replace " +\d+$", ""
+    return "$startFolder\$torrentFolder\$torrentFileName"
+}
+function GetTorrentExpectedContents([string] $startFolder, [string] $torrentFile)
+{
+    $torrentFileContentLines=cmd /c ".\dumptorrent-1.2.exe" "$startFolder\$torrentFile"
+    $startedFiles =$false
+    [System.Collections.ArrayList] $torrentFileContent=@("")
+    $torrentFolder=""
+    foreach($torrentFileContentLine in $torrentFileContentLines)
+    {
+        if($torrentFileContentLine.StartsWith("Name:"))
+        {
+            $torrentFolder=$torrentFileContentLine.Replace("Name:           ","")
+        }
+        if($torrentFileContentLine -eq "Files:")
+        {
+            $startedFiles=$true
+        }
+        if($torrentFileContentLine -ne "Files:" -And $startedFiles)
+        {
+            $torrentFileName=GetFileName $startFolder $torrentFolder $torrentFileContentLine.Trim()
+            [void]$torrentFileContent.Add($torrentFileName)
+        }
+    }
+    $retValue = New-Object PSObject -Property @{ TorrentFolder=$torrentFolder; TorrentContent= $torrentFileContent.ToArray()}
+    return $retValue
+}
+
+function FilesMissing([System.Collections.ArrayList] $filesInTorrent)
+{
+    foreach($fileInTorrent in $filesInTorrent)
+    {
+
+        if(([string]::IsNullOrEmpty($fileInTorrent) -ne $true) -And ([System.IO.File]::Exists($fileInTorrent) -ne $true))
+        {
+            return $true
+        }
+    }
+    return $false
+}
+function GetExcludeList()
+{
+    $torrentFiles= Get-ChildItem -Path $startFolder -Filter "*.torrent"
+    [System.Collections.ArrayList] $foldersToExclude = @($targetFolder)
+    foreach($torrentFile in $torrentFiles)
+    {
+        $torrentExpectedContents= GetTorrentExpectedContents $startFolder $torrentFile
+        $inWork=FilesMissing -filesInTorrent $torrentExpectedContents.TorrentContent
+        if($inWork)
+        {
+            [void]$foldersToExclude.Add($torrentExpectedContents.TorrentFolder)
+        }
+    }
+    return $foldersToExclude
+}
+
+$excludeFolders=GetExcludeList
+Write-Host $excludeFolders
+$folders = Get-ChildItem -Path $startFolder -Exclude $excludeFolders | ?{ $_.PSIsContainer }
 $folders|ForEach-Object {Move-Item $_* $startFolder\$targetFolder}
 $zipFiles= Get-ChildItem -Path $startFolder
 $zipFiles |Where-Object {$_.Extension -eq $archiveExtension}|ForEach-Object {Move-Item $_.FullName $startFolder\$targetFolder}
