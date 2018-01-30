5282f1cd7b9828888552724dd7fbf9809cddef4d
oduma <tavi_duma2002@yahoo.co.uk>
2018-01-30 08:24:14 +0000

    Added a signature for post processors

---
+++ Common/Sciendo.Common.Music.Contracts/IPostProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sciendo.Common.Music.Contracts
+{
+    public interface IPostProcessor
+    {
+        void Process(string message);
+    }
+}
