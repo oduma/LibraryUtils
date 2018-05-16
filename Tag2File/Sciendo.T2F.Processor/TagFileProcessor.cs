1663cbdb488bf4caa8485a49dd676e5bcadc10e2
oduma <tavi_duma2002@yahoo.co.uk>
2018-05-16 18:30:56 +0100

    updated sciendo.common and added the grouping by letter

--- Tag2File/Sciendo.T2F.Processor/TagFileProcessor.cs
+++ Tag2File/Sciendo.T2F.Processor/TagFileProcessor.cs
@@ -8,7 +8,19 @@ namespace Sciendo.T2F.Processor
         public string CalculateFileName(Tag input, string rootPath, string extension, string fileNamePattern)
         {
             return
-                $"{rootPath}{Path.DirectorySeparatorChar}{string.Format(MapPattern(fileNamePattern), IOFy(input.Title), IOFy(input.Album), IOFy(string.Join("-", input.AlbumArtists)), IOFy(string.Join("-", input.Artists)), input.Track, input.Disc)}{extension}";
+                $"{rootPath}{Path.DirectorySeparatorChar}{string.Format(MapPattern(fileNamePattern), IOFy(input.Title), IOFy(input.Album), IOFy(string.Join("-", input.AlbumArtists)), IOFy(string.Join("-", input.Artists)), input.Track, input.Disc,CalculateCategory(input.Artists))}{extension}";
+        }
+
+        private string CalculateCategory(string[] inputArtists)
+        {
+            if (char.IsLetter(inputArtists[0][0]))
+            {
+                return inputArtists[0].Substring(0, 1);
+            }
+            else
+            {
+                return "0-9";
+            }
         }
 
         private object IOFy(string input)
@@ -24,7 +36,8 @@ namespace Sciendo.T2F.Processor
                     .Replace("%l", "{1}")
                     .Replace("%t", "{0}")
                     .Replace("%n", "{4}")
-                    .Replace("%d", "{5}");
+                    .Replace("%d", "{5}")
+                    .Replace("%z","{6}");
         }
     }
 }
