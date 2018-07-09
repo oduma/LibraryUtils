e10cdd1c91d6e5aeef157df39da6605919915030
oduma <tavi_duma2002@yahoo.co.uk>
2018-07-09 23:06:00 +0100

    Fixes applyied gallore

--- Playlist.Persister/Sciendo.Playlist.Persister/PersisterProcessor.cs
+++ Playlist.Persister/Sciendo.Playlist.Persister/PersisterProcessor.cs
@@ -35,46 +35,76 @@ namespace Sciendo.Playlist.Persister
 
                 foreach (var file in files)
                 {
-                    ProcessFile(file, outPath);
+                    var targetDirectory = ProcessFile(file, outPath);
+                    //if (!string.IsNullOrEmpty(targetDirectory) && _targetStorage is MtpStorage)
+                    //{
+                    //    CopyToDevice(file, targetDirectory, outPath);
+                    //}
                 }
             }
             if (File.Exists(inPath))
             {
                 StartProcessing?.Invoke(this, new ProgressEventArgs(inPath, 1));
-                ProcessFile(inPath, outPath);
+                var targetDirectory = ProcessFile(inPath, outPath);
+                //if (!string.IsNullOrEmpty(targetDirectory) && _targetStorage is MtpStorage)
+                //{
+                //    CopyToDevice(inPath, targetDirectory, outPath);
+                //}
             }
-
         }
 
-        private void ProcessFile(string file, string outPath)
+        private string ProcessFile(string file, string outPath)
         {
             var inPlaylist = ReadPlaylist(file);
             if (inPlaylist != null)
             {
                 StartProcessingFile?.Invoke(this, new ProgressEventArgs(file,inPlaylist.Length));
-                var targetDirectory = HandleContent(file, inPlaylist, outPath);
-                CreateTargetPlaylist(inPlaylist, file, targetDirectory);
+                string targetDirectory = string.Empty;
+                if (_targetStorage is MtpStorage)
+                {
+                    targetDirectory = HandleContent(file, inPlaylist, AppDomain.CurrentDomain.BaseDirectory,_sourceStorage);
+                    CreateTargetPlaylist(inPlaylist, file, targetDirectory,_sourceStorage);
+                    return targetDirectory;
+                }
+                else
+                {
+                    targetDirectory = HandleContent(file, inPlaylist, outPath, _targetStorage);
+                    CreateTargetPlaylist(inPlaylist, file, targetDirectory,_targetStorage);
+                    return targetDirectory;
+                }
             }
             else
             {
                 StartProcessingFile?.Invoke(this, new ProgressEventArgs(file,0));
+                return string.Empty;
             }
         }
 
-        private void CreateTargetPlaylist(PlaylistItem[] inPlaylist, string sourceFile, string targetDirectory)
+        private void CopyToDevice(string inPlaylistName, string fromDirectory, string toDirectory)
+        {
+            var targetDirectory = CreateTargetDirectory(inPlaylistName, toDirectory, _targetStorage);
+            foreach (var sourceFile in _sourceStorage.Directory.GetFiles(fromDirectory,SearchOption.AllDirectories))
+            {
+                var targetFile = GetTargetFilePath(targetDirectory, sourceFile);
+                StartProcessingFile?.Invoke(this, new ProgressEventArgs(targetFile, 0));
+                _targetStorage.File.Create(targetFile, _sourceStorage.File.Read(sourceFile));
+            }
+        }
+
+        private void CreateTargetPlaylist(PlaylistItem[] inPlaylist, string sourceFile, string targetDirectory,IStorage storage)
         {
             var playlistHandler = PlaylistHandlerFactory.GetHandler(_targetPlaylistType.ToString().ToLower());
 
-            var newPlaylistRawContent = playlistHandler.SetPlaylistItems(_targetStorage.File, inPlaylist,targetDirectory);
+            var newPlaylistRawContent = playlistHandler.SetPlaylistItems(storage.File, inPlaylist,targetDirectory);
             var targetFileName = $"{Path.GetFileNameWithoutExtension(sourceFile)}.{_targetPlaylistType.ToString().ToLower()}";
-            _targetStorage.File.WriteAllText(Path.Combine(targetDirectory, targetFileName), newPlaylistRawContent);
+            storage.File.WriteAllText(Path.Combine(targetDirectory, targetFileName), newPlaylistRawContent);
             PlaylistCreated?.Invoke(this, new ProgressEventArgs(targetFileName,1));
         }
 
-        private string HandleContent(string file, PlaylistItem[] inPlaylist, string outPath)
+        private string HandleContent(string file, PlaylistItem[] inPlaylist, string outPath, IStorage storage)
         {
-            var targetDirectory = CreateTargetDirectory(file, outPath);
-            var countFiles = CopyContent(targetDirectory, inPlaylist);
+            var targetDirectory = CreateTargetDirectory(file, outPath, storage);
+            var countFiles = CopyContent(targetDirectory, inPlaylist, storage);
             CopyContentToTarget?.Invoke(this, new ProgressEventArgs(targetDirectory,countFiles));
             return targetDirectory;
         }
@@ -93,7 +123,7 @@ namespace Sciendo.Playlist.Persister
             return null;
         }
 
-        private int CopyContent(string targetDirectory, PlaylistItem[] playlistItems)
+        private int CopyContent(string targetDirectory, PlaylistItem[] playlistItems, IStorage storage)
         {
             int fileCopyCount = 0;
             foreach (var playlistItem in playlistItems)
@@ -101,7 +131,7 @@ namespace Sciendo.Playlist.Persister
                 var sourceFile = playlistItem.FileName;
                 var targetFile = GetTargetFilePath(targetDirectory, sourceFile, ++fileCopyCount);
                 StartProcessingFile?.Invoke(this, new ProgressEventArgs(targetFile, 0));
-                _targetStorage.File.Create(targetFile,_sourceStorage.File.Read(sourceFile));
+                storage.File.Create(targetFile,_sourceStorage.File.Read(sourceFile));
                 playlistItem.FileName = Path.GetFileName(targetFile);
             }
             return fileCopyCount;
@@ -112,26 +142,26 @@ namespace Sciendo.Playlist.Persister
             throw new NotImplementedException();
         }
 
-        private string GetTargetFilePath(string targetDirectory, string sourceFile, int indexInPlaylist)
+        private string GetTargetFilePath(string targetDirectory, string sourceFile, int indexInPlaylist=0)
         {
             var targetExtension = Path.GetExtension(sourceFile);
 
             var targetFileName = Path.GetFileNameWithoutExtension(sourceFile);
-            if (_targetStorage is MtpStorage)
+            if (_targetStorage is MtpStorage && indexInPlaylist>0)
                 targetFileName = indexInPlaylist.ToString().PadLeft(3, '0');
             var targetFile = $"{targetFileName}{targetExtension}";
             return Path.Combine(targetDirectory, targetFile);
         }
 
-        private string CreateTargetDirectory(string playlistFileName, string outPath)
+        private string CreateTargetDirectory(string playlistFileName, string outPath, IStorage storage)
         {
             var fileName = Path.GetFileName(playlistFileName);
             if (fileName != null)
             {
                 var targetDirectory =
                     $"{outPath}{Path.DirectorySeparatorChar}{fileName.Replace(Path.GetExtension(fileName), "")}";
-                if (!_targetStorage.Directory.Exists(targetDirectory))
-                    _targetStorage.Directory.CreateDirectory(targetDirectory);
+                if (!storage.Directory.Exists(targetDirectory))
+                    storage.Directory.CreateDirectory(targetDirectory);
                 return targetDirectory;
             }
            return playlistFileName;
